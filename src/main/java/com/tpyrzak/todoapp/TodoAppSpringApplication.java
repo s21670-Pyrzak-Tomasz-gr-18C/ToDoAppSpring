f8e7f2ccf6e5b7d1c949cd91015bb327f4d57e3f
tpyrzak <Baltazar15>
2021-10-05 11:22:06 +0200

    Initial commit spring boot app

---
+++ src/main/java/com/tpyrzak/todoapp/TodoAppSpringApplication.java
@@ -0,0 +1,13 @@
+package com.tpyrzak.todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppSpringApplication.class, args);
+	}
+
+}
